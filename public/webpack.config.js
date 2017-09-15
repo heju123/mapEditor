d16e9ca46d49e896206cb7a67933eeaeb70026fd
heju <heju@sobey.com>
2017-09-15 19:38:46 +0800

    初始代码提交

---
+++ public/webpack.config.js
@@ -0,0 +1,45 @@
+var compile_mode = "dev";
+
+var cleanWebpackPlugin = require('clean-webpack-plugin');
+var uglifyJsPlugin = require('uglify-js-plugin');
+
+var devtool = "source-map";
+var plugins = [];
+plugins.push(new cleanWebpackPlugin(['build/*.{js,map}', 'build/images/*.{png,jpg,jpeg,gif}'],
+    {
+        root: '',
+        verbose: true,
+        dry: false
+    }));
+if (compile_mode == "prod") {
+    plugins.push(new uglifyJsPlugin({
+        compress: true, //default 'true', you can pass 'false' to disable this plugin
+        debug: true, //default 'false', it will display some information in console
+        sourceMap: true
+    }));
+    devtool = "cheap-module-source-map";
+}
+
+module.exports = {
+    entry: __dirname + '/src/js/main.js',
+    output: {
+        path: __dirname + '/build',
+        publicPath : "/build/",
+        filename: "bundle.js",
+        chunkFilename: '[name].chunk.js'
+    },
+    devtool: devtool,  //生成source file
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
+    plugins: plugins
+};
